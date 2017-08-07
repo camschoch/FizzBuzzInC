39547d83cf77f80834444634bf309e14482a4187
camschoch <camschoch3@gmail.com>
2017-08-07 07:21:06 -0500

    first commit

---
+++ FizzBuzz/MakingFizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class MakingFizzBuzz
+    {
+        public void FizzBuzz()
+        {
+            for (var number = 0; number <= 100; number++)
+            {
+
+                if (number == 0)
+                {
+                    Console.WriteLine(0);
+            
+                }
+
+                else if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+            
+                }
+
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(number);
+            
+                }
+            }
+        }
+    }
+}
